ef4713fc8fc28a162239068c5cd6c833465dab46
GeorginaSarmiento <sarmiento.ga@obmontessori.edu.ph>
2026-09-09 14:52:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 from pyscript import display, document
 
 def create_order(e):
-    document.getElementById('result').innerHTML="  "
+    document.getElementById('show').innerHTML="  "
     prod1 = document.getElementById('item1')
     prod2 = document.getElementById('item2')
     prod3 = document.getElementById('item3')
     prod4 = document.getElementById('item4')
     prod5 = document.getElementById('item5')
 
-    subtotal = float(prod1.value) * prod1.checked
-    # subtotal = float(prod2.value) * prod2.checked
-    # subtotal = float(prod3.value) * prod3.checked
-    # subtotal = float(prod4.value) * prod4.checked
-    # subtotal = float(prod5.value) * prod5.checked
-    display(f'Your payment is {subtotal}', target='show')
+    subtotal = float(prod1.value) * prod1.checked + float(prod2.value) * prod2.checked + float(prod3.value) * prod3.checked + float(prod4.value) * prod4.checked + float(prod5.value) * prod5.checked
+    display(f'Your payment is ₱ {subtotal}', target='show')
